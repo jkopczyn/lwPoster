a2d2c9e4ef9307693556a50b9797dd67c35ecf07
Jacob Kopczynski <ja.kopczynski@gmail.com>
2024-06-14 17:13:49 -0700

    add interactive insert-or-update
     checks for conflicts and prompts to continue before overriding

--- lib/datastore.py
+++ lib/datastore.py
@@ -26,7 +26,7 @@ def lookup_with_overrides(db, rich_text = None, series = None, topic = None):
         topic_q = q
     else:
         topic_q = q.topic == topic
-    
+
     blank_q = (~q.rich_text.exists() & ~q.meetup_series.exists() & ~q.topic.exists())
     defaults = db.search(blank_q)
 
@@ -47,7 +47,7 @@ def lookup_with_overrides(db, rich_text = None, series = None, topic = None):
 def merged_query(db, query):
     """Query a database or table and merge all results into one
 
-    :param db (TinyDB Database/Table): table/db to search
+    :param db (TinyDB DB or Table): database or table to look up in
     :param query (TinyDB QueryInstance): search to execute
     """
     defaults = {}
@@ -56,3 +56,48 @@ def merged_query(db, query):
         defaults.update(r)
 
     return defaults
+
+
+def interactive_insert_data(db, record):
+    """Insert/Override data in the DB, prompting user for confirmation
+
+    :param db (TinyDB DB or Table): database or table to look up in
+    :param record (dict): key-value pairs to add to DB
+    """
+    organizational_keys = ["rich_text", "meetup_series", "topic", "location"]
+    subrecord = {}
+    for k in organizational_keys:
+        x = record.get(k)
+        if x is not None:
+            subrecord[k] = x
+
+    conflicts = {}
+    existing_records = db.search(Query().fragment(subrecord))
+    for k in record.keys():
+        if k in organizational_keys:
+            continue
+        for r in existing_records:
+            if r.get(k) is not None:
+                if conflicts.get(k) is None:
+                    conflicts[k] = []
+                conflicts[k].append(r[k])
+        if conflicts.get(k) is not None:
+            conflicts[k].append(record[k])
+    if len(conflicts) > 0:
+        print("new records conflict with existing data:\n %s\n" % conflicts)
+        cont_input = input("continue, overriding old data? (y/N) ")
+        if not coerce_bool_input(cont_input):
+            return
+    return db.upsert(record, Query().fragment(subrecord))
+
+
+def coerce_bool_input(inpt, default=False):
+    coerced = inpt.strip().lower()
+    if coerced == "y" or coerced == "yes":
+        b = True
+    elif coerced == "n" or coerced == "no" or coerced == "":
+        b = False
+    else:
+        print("Didn't understand response, defaulting to no")
+        b = False
+    return b
