a604b0e73a54efa6e790f60ba67caff7dc2019cd
Jacob Kopczynski <ja.kopczynski@gmail.com>
2024-06-28 14:02:01 -0700

    depseusdo the code for templating

--- lib/templating.py
+++ lib/templating.py
@@ -1,18 +1,18 @@
-import regex
+import re
 import string
 
-def template_pass(template, params):
-    match_pairs = regex.match('${([^}]*)}', template)
+def _template_pass(template, params):
+    match_pairs = re.findall('(\${([^}]*)})', template)
     any_changes = False
     for (mtch, name) in match_pairs:
         if name not in params:
             raise ValueError("Template required not-provided param %s." % name)
-        template = string.replace(template, mtch, params[name])
+        template = template.replace(mtch, params[name])
         any_changes = True
     return (template, any_changes)
 
 def fill_template(template, params):
-    template, another = template_pass(template, params)
+    template, another = _template_pass(template, params)
     while another:
-        template, another = template_pass(template, params)
+        template, another = _template_pass(template, params)
     return template
