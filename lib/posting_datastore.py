c03549a1cf9f0e14ac3bfa6ffec24870c8d1ccda
Jacob Kopczynski <ja.kopczynski@gmail.com>
2024-07-04 17:22:17 -0700

    force it to match apis for get/set?

--- lib/posting_datastore.py
+++ lib/posting_datastore.py
@@ -15,3 +15,22 @@ class DatastorePostingConfig:
     def from_dict(cls, d):
         raise TypeError("Datastore can't be initialized from dict")
 
+    def get(self, *list_of_keys):
+        # this doesn't really work on the same API
+
+    def get_default(self, *args):
+        if len(args) < 2:
+            raise KeyError
+        default = args[-1]
+        args = args[:-1]
+        try:
+            v = self.get(*args)
+            if v != None:
+                return v
+        except KeyError:
+            pass
+        v = default
+        self.set(*args, v)
+        return v
+
+
